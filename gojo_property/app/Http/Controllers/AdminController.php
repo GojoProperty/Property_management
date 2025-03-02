9f16fe157b839834a9fa856c0038e1380beced8a
hermiZe <hermelakew1995@gmail.com>
2025-03-02 08:02:39 -1200

    admin profile commit

--- gojo_property/app/Http/Controllers/AdminController.php
+++ gojo_property/app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
 
@@ -27,5 +28,29 @@ class AdminController extends Controller
     public function AdminLogin()
     {
         return view('admin.admin_login');
+    }
+    public function AdminProfile()
+    {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_profile_view', compact('profileData'));
+    }
+    public function AdminProfileStore(Request $request)
+    {
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->username = $request->username;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            $fileName = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'), $fileName);
+            $data['photo'] = $fileName;
+        }
+        $data->save();
+        return redirect()->back();
     }
 }
