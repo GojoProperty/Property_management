e035d8b8ddff47fcf90eb9bff833cbdae086a976
darartuamanu <darartuamanu6@gmail.com>
2025-03-07 17:26:37 +0300

    propertyType  controller and adding property type sidebar

--- gojo_property/routes/web.php
+++ gojo_property/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AgentController;
+use App\Http\Controllers\Backend\propertyTypeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -37,3 +38,14 @@ Route::middleware(['auth', 'role:agent'])->group(function () {
 }); //agent page
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+/// Admin Group Middleware 
+Route::middleware(['auth','role:admin'])->group(function(){ 
+
+    // Property Type All  
+Route::controller(PropertyTypeController::class)->group(function(){
+        Route::get('/all/type', 'AllType')->name('all.type');  
+   
+   });
+   
+   
+   }); 
