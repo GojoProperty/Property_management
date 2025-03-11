af0cc76d4ac8d937994a22db6d5187221a54408c
hermiZe <hermelakew1995@gmail.com>
2025-03-11 08:12:00 -1200

    create amneties table and add its functionality

--- gojo_property/routes/web.php
+++ gojo_property/routes/web.php
@@ -34,16 +34,15 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
 
     // Property Type Routes
     Route::controller(PropertyTypeController::class)->group(function () {
+        // property type functionality
         Route::get('/all/type', 'AllType')->name('all.type');
         Route::get('/add/type', 'AddType')->name('add.type');
         Route::post('/store/type', 'StoreType')->name('store.type');
         Route::get('/edit/type/{id}', 'EditType')->name('edit.type');
-
         Route::post('/update/type', 'UpdateType')->name('update.type');
         Route::get('/delete/type/{id}', 'DeleteType')->name('delete.type');
-    });
-    Route::controller(PropertyTypeController::class)->group(function () {
 
+        //property ameneties functionality
         Route::get('/all/amenitie', 'AllAmenitie')->name('all.amenitie');
         Route::get('/add/amenitie', 'AddAmenitie')->name('add.amenitie');
         Route::post('/store/type', 'StoreType')->name('store.type');
