a334efb32f5c697454de323c630ecefae375a119
lidyayigeremu <lidyayigeremu7@gmail.com>
2025-03-12 02:18:50 +0300

    User Logout
    Implemented user logout functionality.
    Redirected users to the login page after logging out.

--- gojo_property/app/Http/Controllers/UserController.php
+++ gojo_property/app/Http/Controllers/UserController.php
@@ -47,5 +47,13 @@ public function UserProfile(){
          return redirect()->back()->with($notification);
  
       }// End Method 
- 
+      public function UserLogout(Request $request) {
+          Auth::guard('web')->logout();
+  
+          $request->session()->invalidate();
+  
+          $request->session()->regenerateToken();
+  
+          return redirect('/login');
+      }
     }
