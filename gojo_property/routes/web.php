7b3c9d94a76a7cb535bacf5c31261b00f93fa0cb
hermiZe <hermelakew1995@gmail.com>
2025-03-17 01:12:38 -1200

    property can be activates if its inactive or deactivated if it is active

--- gojo_property/routes/web.php
+++ gojo_property/routes/web.php
@@ -69,6 +69,8 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
         Route::post('/store/new/multiimage', 'storeNewMultiimage')->name('store.new.multiimage');
         Route::post('/update/property/facilities', 'updatePropertyFacilities')->name('update.property.facilities');
         Route::get('/details/property/{id}', 'DetailsProperty')->name('details.property');
+        Route::post('/inactive/property', 'InactiveProperty')->name('inactive.property');
+        Route::post('/active/property', 'ActiveProperty')->name('active.property');
     });
 });
 
