4a5bac28272ce2a1137bb5ea6fc85acdd74b47d1
hemien-me <hemen0635@gmail.com>
2025-04-02 09:06:17 +0300

    Updated routes in web.php

--- gojo_property/routes/web.php
+++ gojo_property/routes/web.php
@@ -21,6 +21,11 @@ Route::get('/dashboard', function () {
 Route::middleware('auth')->group(function () {
     Route::get('/user/profile', [UserController::class, 'UserProfile'])->name('user.profile');
     Route::post('/user/profile/store', [UserController::class, 'UserProfileStore'])->name('user.profile.store');
+    Route::get('/user/logout', [UserController::class, 'UserLogout'])->name('user.logout'); 
+
+    Route::get('/user/change/password', [UserController::class, 'UserChangePassword'])->name('user.change.password'); 
+   
+     Route::post('/user/password/update', [UserController::class, 'UserPasswordUpdate'])->name('user.password.update');
 });
 
 Route::get('/agent/login', [AgentController::class, 'AgentLogin'])->name('agent.login');
