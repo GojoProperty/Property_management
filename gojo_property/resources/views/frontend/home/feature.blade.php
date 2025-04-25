dcd84aaf21a4a142b64632f9ae5c7576491a5067
hermiZe <hermelakew1995@gmail.com>
2025-04-26 00:39:00 +0300

    create wishlistcontroller and migration table

--- gojo_property/resources/views/frontend/home/feature.blade.php
+++ gojo_property/resources/views/frontend/home/feature.blade.php
@@ -58,7 +58,9 @@
                                             </div>
                                             <ul class="other-option pull-right clearfix">
                                                 <li><a href="property-details.html"><i class="icon-12"></i></a></li>
-                                                <li><a href="property-details.html"><i class="icon-13"></i></a></li>
+                                                <li><a aria-label="Add To Wishlist" class="action-btn"
+                                                        id="{{ $item->id }}" onclick="addToWishList(this.id)"><i
+                                                            class="icon-13"></i></a></li>
                                             </ul>
                                         </div>
                                         <p>{{ $item->short_descp }}</p>
@@ -74,6 +76,8 @@
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </div>
             @endforeach
         </div>
         <div class="more-btn centred"><a href="property-list.html" class="theme-btn btn-one">View All Listing</a>
