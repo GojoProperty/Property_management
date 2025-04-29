4c6b0efb440e63c91ca9bee2f52b18e952af7f55
hemien-me <hemen0635@gmail.com>
2025-04-29 10:54:40 +0300

    package invoice
    invoice pdf

--- gojo_property/app/Http/Controllers/Backend/PropertyController.php
+++ gojo_property/app/Http/Controllers/Backend/PropertyController.php
@@ -371,6 +371,7 @@ class PropertyController extends Controller
         );
         return redirect()->route('all.property')->with($notification);
     } // End Method 
+
     public function AdminPackageHistory(){
  
         $packagehistory = PackagePlan::latest()->get();
@@ -378,4 +379,17 @@ class PropertyController extends Controller
    
    
        }// End Method 
+       
+       public function PackageInvoice($id){
+ 
+        $packagehistory = PackagePlan::where('id',$id)->first();
+
+        $pdf = Pdf::loadView('backend.package.package_history_invoice', compact('packagehistory'))->setPaper('a4')->setOption([
+            'tempDir' => public_path(),
+            'chroot' => public_path(),
+        ]);
+        return $pdf->download('invoice.pdf');
+
+    }// End Method
+
 }
