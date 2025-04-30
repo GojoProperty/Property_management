3d03ed97ce02d2607c327796bf22321adb022921
hermiZe <hermelakew1995@gmail.com>
2025-05-01 01:21:03 +0300

    update frontend

--- gojo_property/resources/views/frontend/frontend_dashboard.blade.php
+++ gojo_property/resources/views/frontend/frontend_dashboard.blade.php
@@ -78,11 +78,10 @@
     <script src="{{ asset('frontend/assets/js/jQuery.style.switcher.min.js') }}"></script>
     <script src="{{ asset('frontend/assets/js/jquery-ui.js') }}"></script>
     <script src="{{ asset('frontend/assets/js/nav-tool.js') }}"></script>
-<<<<<<< Updated upstream
+
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA-CE0deH3Jhj6GN4YvdCFZS7DpbXexzGU"></script>
     <script src="{{ asset('frontend/assets/js/gmaps.js') }}"></script>
     <script src="{{ asset('frontend/assets/js/map-helper.js') }}"></script>
-=======
 
     <!-- Leaflet JS -->
     <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
@@ -110,7 +109,6 @@
 
     <!-- Your custom JS (make sure this comes AFTER the plugin) -->
     <script src="{{ asset('frontend/assets/js/script.js') }}"></script>
->>>>>>> Stashed changes
 
     <!-- main-js -->
     <script src="{{ asset('frontend/assets/js/script.js') }}"></script>
