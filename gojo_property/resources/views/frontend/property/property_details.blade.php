1f6212ebcd68db106f89486c9cc547f15c86b458
darartuamanu <darartuamanu6@gmail.com>
2025-05-02 23:14:59 +0300

    Update property_details.blade.php

--- gojo_property/resources/views/frontend/property/property_details.blade.php
+++ gojo_property/resources/views/frontend/property/property_details.blade.php
@@ -117,7 +117,6 @@
                                 <li><span>Address:</span> {{ $property->address }}</li>
                                 <li><span>State/county:</span> {{ $property->state }}</li>
                                 <li><span>Neighborhood:</span> {{ $property->neighborhood }}</li>
-                            
                                 <li><span>City:</span> {{ $property->city }}</li>
                             </ul>
                             <div class="google-map-area">
@@ -289,68 +288,6 @@
                                         @else
                                             <input type="hidden" name="agent_id" value="{{ $property->agent_id }}">
                                         @endif
-
-                                        
-                                        <div class="form-group message-btn">
-                                            <button type="submit" class="theme-btn btn-one">Send Message</button>
-                                        </div>
-                                    </form>
-
-                                @endauth
-
-
-
-                            </div>
-
-
-
->>>>>>> Stashed changes
-                        </div>
-                        <div class="form-inner">
-                            @auth
-                                @php
-                                    $id = Auth::user()->id;
-                                    $userData = App\Models\User::find($id);
-                                @endphp
-
-                                <form action="{{ route('property.message') }}" method="post" class="default-form">
-                                    @csrf
-
-                                    <input type="hidden" name="property_id" value="{{ $property->id }}">
-                                    @if ($property->agent_id == null)
-                                        <input type="hidden" name="agent_id" value="">
-                                    @else
-                                        <input type="hidden" name="agent_id" value="{{ $property->agent_id }}">
-                                    @endif
-
-                                    <div class="form-group">
-                                        <input type="text" name="msg_name" placeholder="Your name"
-                                            value="{{ $userData->name }}">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="email" name="msg_email" placeholder="Your Email"
-                                            value="{{ $userData->email }}">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="text" name="msg_phone" placeholder="Phone"
-                                            value="{{ $userData->phone }}">
-                                    </div>
-                                    <div class="form-group">
-                                        <textarea name="message" placeholder="Message"></textarea>
-                                    </div>
-                                    <div class="form-group message-btn">
-                                        <button type="submit" class="theme-btn btn-one">Send Message</button>
-                                    </div>
-                                </form>
-                            @else
-                                <form action="{{ route('property.message') }}" method="post" class="default-form">
-                                    @csrf
-                                    <input type="hidden" name="property_id" value="{{ $property->id }}">
-                                    @if ($property->agent_id == null)
-                                        <input type="hidden" name="agent_id" value="">
-                                    @else
-                                        <input type="hidden" name="agent_id" value="{{ $property->agent_id }}">
-                                    @endif
 
                                         
                                         <div class="form-group message-btn">
