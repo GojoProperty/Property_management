328d65d40df9d7f5450a9926ae4691877b595583
hermiZe <hermelakew1995@gmail.com>
2025-05-13 23:15:29 +0300

    Update web.php

--- gojo_property/routes/web.php
+++ gojo_property/routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AgentController;
-
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\RedirectIfAuthenticated;
 use App\Http\Controllers\AdminController;
