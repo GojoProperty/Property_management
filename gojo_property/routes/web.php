1fa16ad43dfc0ec271fd72444eb9ce9aaefc876a
hermiZe <hermelakew1995@gmail.com>
2025-05-22 03:14:45 +0300

    Update web.php

--- gojo_property/routes/web.php
+++ gojo_property/routes/web.php
@@ -49,7 +49,7 @@ Route::middleware('auth')->group(function () {
 });
 
 //login and register route
-Route::get('/agent/login', [AgentController::class, 'AgentLogin'])->name('agent.login')->middleware(RedirectIfAuthenticated::class);
+Route::get('/agent/login', [AgentController::class, 'AgentLogin'])->name('agent.login');
 Route::post('/agent/register', [AgentController::class, 'AgentRegister'])->name('agent.register');
 Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login')->middleware('redirect.authenticated');
 
@@ -180,7 +180,11 @@ Route::middleware(['auth', 'role:agent'])->group(function () {
         Route::get('/agent/delete/property/{id}', 'AgentDeleteProperty')->name('agent.delete.property');
         Route::get('/agent/message/details/{id}', 'AgentMessageDetails')->name('agent.message.details');
 
+        // Schedule Request Route
+        Route::get('/agent/schedule/request',  'AgentScheduleRequest')->name('agent.schedule.request');
+        Route::get('/agent/details/schedule/{id}', 'AgentDetailsSchedule')->name('agent.details.schedule');
 
+        Route::post('/agent/update/schedule', 'AgentUpdateSchedule')->name('agent.update.schedule');
 
 
         // Buy Package
@@ -192,35 +196,29 @@ Route::middleware(['auth', 'role:agent'])->group(function () {
         Route::get('/package/history', 'PackageHistory')->name('package.history');
         Route::get('/agent/package/invoice/{id}', 'AgentPackageInvoice')->name('agent.package.invoice');
     });
-
-
-    // Schedule Request Route
-    Route::get('/agent/schedule/request', [AgentPropertyController::class, 'AgentScheduleRequest'])->name('agent.schedule.request');
-    Route::get('/agent/details/schedule/{id}', [AgentPropertyController::class, 'AgentDetailsSchedule'])->name('agent.details.schedule');
-
-    Route::post('/agent/update/schedule', [AgentPropertyController::class, 'AgentUpdateSchedule'])->name('agent.update.schedule');
 });
-
+Route::controller(IndexController::class)->group(function () {
+    Route::post('/property/message', 'PropertyMessage')->name('property.message');
+    // Agent Details Page in Frontend 
+    Route::get('/agent/details/{id}', 'AgentDetails')->name('agent.details');
+    // Send Message from Agent Details Page 
+    Route::post('/agent/details/message', 'AgentDetailsMessage')->name('agent.details.message');
+    // Get All Rent Property 
+    Route::get('/rent/property', 'RentProperty')->name('rent.property');
+    // Get All Buy Property 
+    Route::get('/buy/property',  'BuyProperty')->name('buy.property');
+    // Get All Property Type Data 
+    Route::get('/property/type/{id}',  'PropertyType')->name('property.type');
+    // Get State Details Data 
+    Route::get('/state/details/{id}',  'StateDetails')->name('state.details');
+    // Home Page Buy Seach Option
+    Route::post('/buy/property/search',  'BuyPropertySearch')->name('buy.property.search');
+    // Home Page Rent Seach Option
+    Route::post('/rent/property/search',  'RentPropertySeach')->name('rent.property.search');
+    // All Property Seach Option
+    Route::post('/all/property/search', 'AllPropertySeach')->name('all.property.search');
+});
 // ===================== Frontend Routes =====================
 Route::get('/property/details/{id}/{slug}', [IndexController::class, 'PropertyDetails']);
 Route::post('/add-to-wishlist/{property_id}', [WishlistController::class, 'AddToWishList']);
 Route::post('/add-to-compare/{property_id}', [CompareController::class, 'AddToCompare']);
-Route::post('/property/message', [IndexController::class, 'PropertyMessage'])->name('property.message');
-// Agent Details Page in Frontend 
-Route::get('/agent/details/{id}', [IndexController::class, 'AgentDetails'])->name('agent.details');
-// Send Message from Agent Details Page 
-Route::post('/agent/details/message', [IndexController::class, 'AgentDetailsMessage'])->name('agent.details.message');
-// Get All Rent Property 
-Route::get('/rent/property', [IndexController::class, 'RentProperty'])->name('rent.property');
-// Get All Buy Property 
-Route::get('/buy/property', [IndexController::class, 'BuyProperty'])->name('buy.property');
-// Get All Property Type Data 
-Route::get('/property/type/{id}', [IndexController::class, 'PropertyType'])->name('property.type');
-// Get State Details Data 
-Route::get('/state/details/{id}', [IndexController::class, 'StateDetails'])->name('state.details');
-// Home Page Buy Seach Option
-Route::post('/buy/property/search', [IndexController::class, 'BuyPropertySearch'])->name('buy.property.search');
-// Home Page Rent Seach Option
-Route::post('/rent/property/search', [IndexController::class, 'RentPropertySeach'])->name('rent.property.search');
-// All Property Seach Option
-Route::post('/all/property/search', [IndexController::class, 'AllPropertySeach'])->name('all.property.search');
