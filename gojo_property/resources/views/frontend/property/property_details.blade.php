07cad25fb635ecac4d575f0a3128823220e60efa
hermiZe <hermelakew1995@gmail.com>
2025-05-30 00:12:42 +0300

    update property detail page

--- gojo_property/resources/views/frontend/property/property_details.blade.php
+++ gojo_property/resources/views/frontend/property/property_details.blade.php
@@ -21,11 +21,11 @@
     </section>
     <!--End Page Title-->
 
-
     <!-- property-details -->
     <section class="property-details property-details-one">
         <div class="auto-container">
             <div class="top-details clearfix">
+                <!-- Left column content remains the same -->
                 <div class="left-column pull-left clearfix">
                     <h3>{{ $property->property_name }}</h3>
                     <div class="author-info clearfix">
@@ -50,6 +50,8 @@
                         </ul>
                     </div>
                 </div>
+
+                <!-- Right column content remains the same -->
                 <div class="right-column pull-right clearfix">
                     <div class="price-inner clearfix">
                         <ul class="category clearfix pull-left">
@@ -68,9 +70,12 @@
                     </ul>
                 </div>
             </div>
+
             <div class="row clearfix">
+                <!-- Main Content Column (8 columns) -->
                 <div class="col-lg-8 col-md-12 col-sm-12 content-side">
                     <div class="property-details-content">
+                        <!-- Image Carousel -->
                         <div class="carousel-inner">
                             <div class="single-item-carousel owl-carousel owl-theme owl-dots-none">
                                 @foreach ($multiImage as $img)
@@ -79,6 +84,8 @@
                                 @endforeach
                             </div>
                         </div>
+
+                        <!-- Property Description -->
                         <div class="discription-box content-widget">
                             <div class="title-box">
                                 <h4>Property Description</h4>
@@ -87,6 +94,8 @@
                                 <p>{!! $property->long_descp !!}</p>
                             </div>
                         </div>
+
+                        <!-- Property Details -->
                         <div class="details-box content-widget">
                             <div class="title-box">
                                 <h4>Property Details</h4>
@@ -100,16 +109,20 @@
                                 <li>Property Size: <span>{{ $property->property_size }} Sq Ft</span></li>
                             </ul>
                         </div>
+
+                        <!-- Amenities -->
                         <div class="amenities-box content-widget">
                             <div class="title-box">
                                 <h4>Amenities</h4>
                             </div>
-                            <ul>
+                            <ul class="amenities-list clearfix">
                                 @foreach ($property_amen as $amen)
                                     <li>{{ $amen }}</li>
                                 @endforeach
                             </ul>
                         </div>
+
+                        <!-- Location -->
                         <div class="location-box content-widget">
                             <div class="title-box">
                                 <h4>Location</h4>
@@ -124,9 +137,11 @@
                                 <div id="map" style="height: 400px; width: 100%; border-radius: 10px;"></div>
                             </div>
                         </div>
+
+                        <!-- Nearby -->
                         <div class="nearby-box content-widget">
                             <div class="title-box">
-                                <h4>What’s Nearby?</h4>
+                                <h4>What's Nearby?</h4>
                             </div>
                             <div class="inner-box">
                                 <div class="single-item">
@@ -136,7 +151,7 @@
                                         @foreach ($facility as $item)
                                             <div class="box clearfix">
                                                 <div class="text pull-left">
-                                                    <h6>{{ $item->facility_name }} <span>({{ $item->distance }} )</span>
+                                                    <h6>{{ $item->facility_name }} <span>({{ $item->distance }})</span>
                                                     </h6>
                                                 </div>
                                             </div>
@@ -146,21 +161,24 @@
                             </div>
                         </div>
 
-                        <div class="statistics-box content-widget"><!--video -->
+                        <!-- Property Video -->
+                        <div class="statistics-box content-widget">
                             <div class="title-box">
-                                <h4>Property Video </h4>
+                                <h4>Property Video</h4>
                             </div>
                             <figure class="image-box">
-                                <iframe width="560" height="315" src=" {{ $property->property_video }}"
+                                <iframe width="100%" height="400" src="{{ $property->property_video }}"
                                     title="YouTube video player" frameborder="0"
                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                     referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                             </figure>
                         </div>
-                        <div class="amenities-box content-widget">
-                            @if ($property->rules->count())
+
+                        <!-- Terms & Conditions -->
+                        @if ($property->rules->count())
+                            <div class="amenities-box content-widget">
                                 <div class="title-box">
-                                    <h4> Terms & Conditions</h4>
+                                    <h4>Terms & Conditions</h4>
                                 </div>
                                 <div class="card-body">
                                     <ul>
@@ -169,20 +187,19 @@
                                         @endforeach
                                     </ul>
                                 </div>
-                            @endif
-                        </div>
+                            </div>
+                        @endif
 
+                        <!-- Schedule Site Visit -->
                         <div class="schedule-box content-widget">
                             <div class="title-box">
-                                <h4>Schedule Site Visit </h4>
+                                <h4>Schedule Site Visit</h4>
                             </div>
                             <div class="form-inner">
                                 <form action="{{ route('store.schedule') }}" method="post">
                                     @csrf
                                     <div class="row clearfix">
-
                                         <input type="hidden" name="property_id" value="{{ $property->id }}">
-
                                         @if ($property->agent_id == null)
                                             <input type="hidden" name="agent_id" value="">
                                         @else
@@ -202,7 +219,6 @@
                                                 <input type="text" name="tour_time" placeholder="Any Time">
                                             </div>
                                         </div>
-
                                         <div class="col-lg-12 col-md-12 col-sm-12 column">
                                             <div class="form-group">
                                                 <textarea name="message" placeholder="Your message"></textarea>
@@ -219,7 +235,10 @@
                         </div>
                     </div>
                 </div>
+
+                <!-- Sidebar Column (4 columns) -->
                 <div class="col-lg-4 col-md-12 col-sm-12 sidebar-side">
+                    <!-- Author Widget -->
                     <div class="property-sidebar default-sidebar">
                         <div class="author-widget sidebar-widget">
                             <div class="author-box">
@@ -227,7 +246,7 @@
                                     <figure class="author-thumb"><img src="{{ url('upload/heriadmin.jpg') }}"
                                             alt=""></figure>
                                     <div class="inner">
-                                        <h4>Admin </h4>
+                                        <h4>Admin</h4>
                                         <ul class="info clearfix">
                                             <li><i class="fas fa-map-marker-alt"></i> Hawassa University</li>
                                             <li><i class="fas fa-phone"></i><a href="tel:03030571965">+251-946948447</a>
@@ -251,6 +270,8 @@
                                 @endif
                             </div>
                         </div>
+
+                        <!-- Contact Form -->
                         <div class="form-inner">
                             @auth
                                 @php
@@ -260,7 +281,6 @@
 
                                 <form action="{{ route('property.message') }}" method="post" class="default-form">
                                     @csrf
-
                                     <input type="hidden" name="property_id" value="{{ $property->id }}">
                                     @if ($property->agent_id == null)
                                         <input type="hidden" name="agent_id" value="">
@@ -314,71 +334,68 @@
                                     </div>
                                 </form>
                             @endauth
-
-
-
                         </div>
 
-
-
-                    </div>
-                    <div class="calculator-widget sidebar-widget">
-                        <!-- ========== Property Request Section ========== -->
-                        <div class="property-request-section" style="margin-top: 40px; margin-bottom: 60px;">
-                            <div class="calculate-inner">
-                                <div class="widget-title">
-                                    <h4>Request This Property</h4>
-                                </div>
-
-                                @auth
-                                    <form method="post"
-                                        action="{{ $property->property_status == 'buy' ? route('purchase.request') : route('rent.request') }}"
-                                        class="default-form">
-                                        @csrf
-                                        <input type="hidden" name="property_id" value="{{ $property->id }}">
-
-                                        <div class="form-group">
-                                            <label>Property Price</label>
-                                            <input type="text" value="{{ $property->max_price }} ETB"
-                                                class="form-control" readonly>
-                                        </div>
-
-                                        <div class="form-group message-btn">
-                                            @if ($property->property_status == 'buy')
-                                                <button type="submit" class="btn btn-success w-100">Buy Now</button>
-                                            @elseif ($property->property_status == 'rent')
-                                                <button type="submit" class="btn btn-info w-100">Rent Now</button>
-                                            @endif
-                                        </div>
-                                    </form>
-                                @else
-                                    <div class="alert alert-warning">
-                                        <strong>Notice:</strong> Please <a href="{{ route('login') }}">Login</a> to make a
-                                        transaction request.
+                        <!-- Property Request Section -->
+                        <div class="calculator-widget sidebar-widget">
+                            <div class="property-request-section" style="margin-top: 40px; margin-bottom: 60px;">
+                                <div class="calculate-inner">
+                                    <div class="widget-title">
+                                        <h4>Request This Property</h4>
                                     </div>
-                                @endauth
+
+                                    @auth
+                                        <form method="post"
+                                            action="{{ $property->property_status == 'buy' ? route('purchase.request') : route('rent.request') }}"
+                                            class="default-form">
+                                            @csrf
+                                            <input type="hidden" name="property_id" value="{{ $property->id }}">
+
+                                            <div class="form-group">
+                                                <label>Property Price</label>
+                                                <input type="text" value="{{ $property->max_price }} ETB"
+                                                    class="form-control" readonly>
+                                            </div>
+
+                                            <div class="form-group message-btn">
+                                                @if ($property->property_status == 'buy')
+                                                    <button type="submit" class="btn btn-success w-100">Buy Now</button>
+                                                @elseif ($property->property_status == 'rent')
+                                                    <button type="submit" class="btn btn-info w-100">Rent Now</button>
+                                                @endif
+                                            </div>
+                                        </form>
+                                    @else
+                                        <div class="alert alert-warning">
+                                            <strong>Notice:</strong> Please <a href="{{ route('login') }}">Login</a> to make a
+                                            transaction request.
+                                        </div>
+                                    @endauth
+                                </div>
                             </div>
                         </div>
                     </div>
+
+                    <!-- Similar Properties -->
                     <div class="similar-content">
                         <div class="title">
                             <h4>Similar Properties</h4>
                         </div>
 
-                        <div class="row clearfix">
-                            @if ($relatedProperty->isEmpty())
-                                <div class="col-12">
-                                    <p>No similar type property is available for now.</p>
-                                </div>
-                            @else
+                        @if ($relatedProperty->isEmpty())
+                            <div class="alert alert-info">
+                                <p>No similar properties available at this time.</p>
+                            </div>
+                        @else
+                            <div class="row">
                                 @foreach ($relatedProperty as $item)
-                                    <div class="col-lg-4 col-md-6 col-sm-12 feature-block">
-                                        <div class="feature-block-one wow fadeInUp animated" data-wow-delay="00ms"
-                                            data-wow-duration="1500ms">
+                                    <div class="col-md-12 mb-4">
+                                        <div class="feature-block-one">
                                             <div class="inner-box">
                                                 <div class="image-box">
                                                     <figure class="image">
-                                                        <img src="{{ asset($item->property_thambnail) }}" alt="">
+                                                        <img src="{{ asset($item->property_thambnail) }}" alt=""
+                                                            class="img-fluid">
                                                     </figure>
                                                     <div class="batch"><i class="icon-11"></i></div>
                                                     <span class="category">{{ $item->type->type_name }}</span>
@@ -389,13 +406,13 @@
                                                             @if ($item->agent_id == null)
                                                                 <figure class="author-thumb">
                                                                     <img src="{{ url('upload/heriadmin.jpg') }}"
-                                                                        alt="">
+                                                                        alt="" class="img-fluid">
                                                                 </figure>
                                                                 <h6>Admin</h6>
                                                             @else
                                                                 <figure class="author-thumb">
                                                                     <img src="{{ !empty($item->user->photo) ? url('upload/agent_images/' . $item->user->photo) : url('upload/no_image.jpg') }}"
-                                                                        alt="">
+                                                                        alt="" class="img-fluid">
                                                                 </figure>
                                                                 <h6>{{ $item->user->name }}</h6>
                                                             @endif
@@ -406,8 +423,7 @@
                                                         </div>
                                                     </div>
                                                     <div class="title-text">
-                                                        <h4>
-                                                            <a
+                                                        <h4><a
                                                                 href="{{ url('property/details/' . $item->id . '/' . $item->property_slug) }}">{{ $item->property_name }}</a>
                                                         </h4>
                                                     </div>
@@ -438,10 +454,12 @@
                                         </div>
                                     </div>
                                 @endforeach
-                            @endif
-                        </div>
+                            </div>
+                        @endif
                     </div>
-                    <!-- ========== End Similar Properties Section ========== -->
-
+                </div>
+            </div>
+        </div>
     </section>
-    <!-- subscribe-section end -->
+    <!-- property-details end -->
+@endsection
