aadedee1535089f7b04b553d941c3522466a8ecf
hermiZe <hermelakew1995@gmail.com>
2025-05-30 17:33:43 +0300

    fix wishlist problem

--- gojo_property/routes/web.php
+++ gojo_property/routes/web.php
@@ -252,7 +252,6 @@ Route::controller(IndexController::class)->group(function () {
     Route::post('/property/message', 'PropertyMessage')->name('property.message');
     Route::post('/store/schedule',  'StoreSchedule')->name('store.schedule');
 
-<<<<<<< Updated upstream
     // Agent Details Page in Frontend 
     Route::get('/agent/details/{id}', 'AgentDetails')->name('agent.details');
     // Send Message from Agent Details Page 
@@ -275,12 +274,6 @@ Route::controller(IndexController::class)->group(function () {
 // ===================== Frontend Routes =====================
 Route::get('/property/details/{id}/{slug}', [IndexController::class, 'PropertyDetails'])->name('property.details');
 Route::post('/add-to-wishlist/{property_id}', [WishlistController::class, 'AddToWishList']);
-=======
-// Frontend Property Details Routes  
-Route::get('/property/details/{id}/{slug}', [IndexController::class, 'PropertyDetails']);
-
-// Compare Add Route 
->>>>>>> Stashed changes
 Route::post('/add-to-compare/{property_id}', [CompareController::class, 'AddToCompare']);
 
 // Blog Details Route 
