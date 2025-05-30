554bea416c096d8e5606e29b488f1fc2353b75b1
hermiZe <hermelakew1995@gmail.com>
2025-05-30 19:11:29 +0300

    organizing compare property page

--- gojo_property/resources/views/frontend/dashboard/wishlist.blade.php
+++ gojo_property/resources/views/frontend/dashboard/wishlist.blade.php
@@ -24,46 +24,18 @@
     <!-- property-page-section -->
     <section class="property-page-section property-list">
         <div class="auto-container">
-            <div class="row clearfix">
-                <div class="col-lg-4 col-md-12 col-sm-12 sidebar-side">
-                    @php
-                        $id = Auth::user()->id;
-                        $userData = App\Models\User::find($id);
-                    @endphp
-                    <div class="blog-sidebar">
-                        <div class="sidebar-widget post-widget">
-                            <div class="widget-title">
-                                <h4>User Profile </h4>
-                            </div>
-                            <div class="post-inner">
-                                <div class="post">
-                                    <figure class="post-thumb"><a href="blog-details.html">
-                                            <img src="{{ !empty($userData->photo) ? url('upload/user_images/' . $userData->photo) : url('upload/no_image.jpg') }}"
-                                                alt=""></a></figure>
-                                    <h5><a href="blog-details.html">{{ $userData->name }} </a></h5>
-                                    <p>{{ $userData->email }} </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="sidebar-widget category-widget">
-                            <div class="widget-title">
-                            </div>
-                            @include('frontend.dashboard.dashboard_sidebar')
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-8 col-md-12 col-sm-12 content-side">
-                    <div class="property-content-side">
-                        <div class="wrapper list">
-                            <div class="deals-list-content list-item">
-                                <div id="wishlist">
+            <div class="col-lg-12 col-md-12 col-sm-12 content-side">
+                <div class="property-content-side">
+                    <div class="wrapper list">
+                        <div class="deals-list-content list-item">
+                            <div id="wishlist">
 
-                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
     </section>
     <!-- property-page-section end -->
