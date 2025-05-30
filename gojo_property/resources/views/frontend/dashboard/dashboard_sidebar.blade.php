09d064bd2fa826d8fc9a37f9429c4e63f943f73d
hermiZe <hermelakew1995@gmail.com>
2025-05-30 17:46:23 +0300

    Update dashboard_sidebar.blade.php

--- gojo_property/resources/views/frontend/dashboard/dashboard_sidebar.blade.php
+++ gojo_property/resources/views/frontend/dashboard/dashboard_sidebar.blade.php
@@ -1,26 +1,27 @@
 <div class="widget-content">
     <ul class="category-list">
 
-        <li class="current"><a href="blog-details.html"><i class="fab fa fa-envelope "></i> Dashboard </a></li>
+        <li><a href="{{ route('user.profile') }}"><i class="fa fa-cog" aria-hidden="true"></i> Settings</a></li>
+        <li><a href="{{ route('user.wishlist') }}"><i class="fa fa-indent" aria-hidden="true"></i> Your Property
+                WishList </a></li>
+
+        <li><a href="{{ route('user.compare') }}"><i class="fa fa-list-alt" aria-hidden="true"></i></i> Properties to
+                Compare </a>
+
+        <li><a href="{{ route('user.schedule.request') }}"><i class="fa fa-credit-card" aria-hidden="true"></i>Your
+                Schedule Requests <span class="badge badge-info">( )</span></a></li>
+
+        </li>
+        <li><a href="{{ route('preferences.create') }}"><i class="fa fa-list-alt" aria-hidden="true"></i></i> Set your
+                preference
+            </a>
+        </li>
         <li>
             <a href="{{ route('user.recommendations') }}">
                 <i class="fas fa-star text-success"></i> Recommendations
             </a>
         </li>
 
-
-        <li><a href="{{ route('user.profile') }}"><i class="fa fa-cog" aria-hidden="true"></i> Settings</a></li>
-        <li><a href="{{ route('user.schedule.request') }}"><i class="fa fa-credit-card" aria-hidden="true"></i>Your
-                Schedule Requests <span class="badge badge-info">( )</span></a></li>
-        <li><a href="{{ route('user.compare') }}"><i class="fa fa-list-alt" aria-hidden="true"></i></i> Properties to
-                Compare </a>
-        </li>
-        <li><a href="{{ route('preferences.create') }}"><i class="fa fa-list-alt" aria-hidden="true"></i></i> Set your
-                preference
-            </a>
-        </li>
-        <li><a href="{{ route('user.wishlist') }}"><i class="fa fa-indent" aria-hidden="true"></i> Your Property
-                WishList </a></li>
         <li><a href="{{ route('user.change.password') }}"><i class="fa fa-key" aria-hidden="true"></i> Security</a></li>
         <li><a href="{{ route('user.logout') }}"><i class="fa fa-chevron-circle-up" aria-hidden="true"></i> Logout</a>
         </li>
