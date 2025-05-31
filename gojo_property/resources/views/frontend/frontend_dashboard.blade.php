c3c503d4f8b8f259947c63ae4593e055ccb7f3f3
hermiZe <hermelakew1995@gmail.com>
2025-05-31 22:48:03 +0300

    Update frontend_dashboard.blade.php

--- gojo_property/resources/views/frontend/frontend_dashboard.blade.php
+++ gojo_property/resources/views/frontend/frontend_dashboard.blade.php
@@ -39,10 +39,10 @@
 <body>
 
     <div class="boxed_wrapper">
-
+       {{-- 
         <!-- preloader -->
         @include('frontend.home.preload')
-        <!-- preloader end -->
+        <!-- preloader end --> --}}
 
         <!-- main header -->
         @include('frontend.home.header')
