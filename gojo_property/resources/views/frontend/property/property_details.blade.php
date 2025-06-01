de97a35f03f87dcfb94635282a506404ab0641e4
hermiZe <hermelakew1995@gmail.com>
2025-06-02 02:26:06 +0300

    authenticated add

--- gojo_property/resources/views/frontend/property/property_details.blade.php
+++ gojo_property/resources/views/frontend/property/property_details.blade.php
@@ -51,18 +51,18 @@
                 <div class="right-column pull-right clearfix">
                     <div class="price-inner clearfix">
                         <ul class="category clearfix pull-left">
-                            <li><a href="property-details.html">{{ $property->type->type_name }}</a></li>
-                            <li><a href="property-details.html">For {{ $property->property_status }}</a></li>
+                            <li>{{ $property->type->type_name }}</li>
+                            <li><a href="#Request_section">For {{ $property->property_status }}</a></li>
                         </ul>
                         <div class="price-box pull-right">
                             <h3>${{ $property->lowest_price }}</h3>
                         </div>
                     </div>
                     <ul class="other-option pull-right clearfix">
-                        <li><a href="property-details.html"><i class="icon-37"></i></a></li>
-                        <li><a href="property-details.html"><i class="icon-38"></i></a></li>
-                        <li><a href="property-details.html"><i class="icon-12"></i></a></li>
-                        <li><a href="property-details.html"><i class="icon-13"></i></a></li>
+                        <li><a aria-label="Compare" class="action-btn" id="{{ $property->id }}"
+                                onclick="addToCompare(this.id)"><i class="icon-12"></i></a></li>
+                        <li><a aria-label="Add To Wishlist" class="action-btn" id="{{ $property->id }}"
+                                onclick="addToWishList(this.id)"><i class="icon-13"></i></a></li>
                     </ul>
                 </div>
             </div>
@@ -342,7 +342,7 @@
                         </div>
 
                         <!-- Property Request Section -->
-                        <div class="calculator-widget sidebar-widget">
+                        <div class="calculator-widget sidebar-widget" id="Request_section">
                             <div class="property-request-section" style="margin-top: 40px; margin-bottom: 60px;">
                                 <div class="calculate-inner">
                                     <div class="widget-title">
