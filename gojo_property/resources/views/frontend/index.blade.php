d11a6f93d773b5bbdec748e072ca0e04b7ce57c7
hermiZe <hermelakew1995@gmail.com>
2025-06-02 00:13:43 +0300

    update header

--- gojo_property/resources/views/frontend/index.blade.php
+++ gojo_property/resources/views/frontend/index.blade.php
@@ -4,57 +4,56 @@
     @include('frontend.home.banner')
     <!-- banner-section end -->
 
-    <!-- category-section -->
-    @include('frontend.home.category')
-    <!-- category-section end -->
+    <div id="category-section">
+        <!-- category-section -->
+        @include('frontend.home.category')
+        <!-- category-section end -->
+    </div>
+    <div id="deals-section">
+        <!-- deals-section -->
+        @include('frontend.home.deals')
+        <!-- deals-section end -->
+    </div>
 
-    <!-- deals-section -->
-    @include('frontend.home.deals')
-    <!-- deals-section end -->
+    <div id="video-section">
+        <!-- video-section -->
+        @include('frontend.home.video')
+        <!-- video-section end -->
+    </div>
 
-    {{-- <!-- feature-section -->
-    @include('frontend.home.feature')
-    <!-- feature-section end --> --}}
-
-
-    <!-- video-section -->
-    @include('frontend.home.video')
-    <!-- video-section end -->
-
-
-
-
-    <!-- testimonial-section end -->
-    @include('frontend.home.testimonial')
-    <!-- testimonial-section end -->
-
-
-    <!-- chooseus-section -->
-    @include('frontend.home.chooseus')
-    <!-- chooseus-section end -->
-
-
-    <!-- place-section -->
-    @include('frontend.home.place')
-    <!-- place-section end -->
-
-
-    <!-- team-section -->
-    @include('frontend.home.team')
-    <!-- team-section end -->
-
-
-    <!-- cta-section -->
-    @include('frontend.home.cta')
-    <!-- cta-section end -->
-
-
-    <!-- news-section -->
-    @include('frontend.home.news')
-    <!-- news-section end -->
-
-
-    <!-- download-section -->
-    @include('frontend.home.download')
-    <!-- download-section end -->
+    <div id="testimonial-section">
+        <!-- testimonial-section end -->
+        @include('frontend.home.testimonial')
+        <!-- testimonial-section end -->
+    </div>
+    <div id="chooseus-section">
+        <!-- chooseus-section -->
+        @include('frontend.home.chooseus')
+        <!-- chooseus-section end -->
+    </div>
+    <div id="place-section">
+        <!-- place-section -->
+        @include('frontend.home.place')
+        <!-- place-section end -->
+    </div>
+    <div id="team-section">
+        <!-- team-section -->
+        @include('frontend.home.team')
+        <!-- team-section end -->
+    </div>
+    <div id="cta-section">
+        <!-- cta-section -->
+        @include('frontend.home.cta')
+        <!-- cta-section end -->
+    </div>
+    <div id="news-section">
+        <!-- news-section -->
+        @include('frontend.home.news')
+        <!-- news-section end -->
+    </div>
+    <div id="download-section">
+        <!-- download-section -->
+        @include('frontend.home.download')
+        <!-- download-section end -->
+    </div>
 @endsection
