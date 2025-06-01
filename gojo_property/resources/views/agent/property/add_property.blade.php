0247a8e62b05280ae2d5f25747264f3c88655c04
hemien-me <hemen0635@gmail.com>
2025-06-01 21:51:59 +0300

    commission

--- gojo_property/resources/views/agent/property/add_property.blade.php
+++ gojo_property/resources/views/agent/property/add_property.blade.php
@@ -43,21 +43,14 @@
                                     </div><!-- Col -->
 
 
+                                    <div class="row">
                                     <div class="col-sm-6">
                                         <div class="form-group mb-3">
-                                            <label class="form-label">Lowest Price </label>
-                                            <input type="text" name="lowest_price" class="form-control">
-                                        </div>
-                                    </div><!-- Col -->
-
-
-                                    <div class="col-sm-6">
-                                        <div class="form-group mb-3">
-                                            <label class="form-label">Max Price </label>
+                                            <label class="form-label">Price </label>
                                             <input type="text" name="max_price" class="form-control">
                                         </div>
                                     </div><!-- Col -->
-
+                                    </div>
 
                                     <div class="col-sm-6">
                                         <div class="form-group mb-3">
