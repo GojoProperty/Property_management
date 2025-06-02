d5e81eac8029fccd9c48fcf87ca418a556500214
hermiZe <hermelakew1995@gmail.com>
2025-06-02 15:22:19 +0300

    Update footer.blade.php

--- gojo_property/resources/views/frontend/home/footer.blade.php
+++ gojo_property/resources/views/frontend/home/footer.blade.php
@@ -16,23 +16,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
-                    <div class="footer-widget links-widget ml-70">
-                        <div class="widget-title">
-                            <h3>Services</h3>
-                        </div>
-                        <div class="widget-content">
-                            <ul class="links-list class">
-                                <li><a href="index.html">About Us</a></li>
-                                <li><a href="index.html">Listing</a></li>
-                                <li><a href="index.html">How It Works</a></li>
-                                <li><a href="index.html">Our Services</a></li>
-                                <li><a href="index.html">Our Blog</a></li>
-                                <li><a href="index.html">Contact Us</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+
                 <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
                     <div class="footer-widget post-widget">
                         <div class="widget-title">
